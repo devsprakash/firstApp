3ee480706c566261fceb14e5e153b85fc2d8f60b
devPrakash7 <psamantaray77@gmail.com>
2022-02-21 09:13:03 +0700

    my first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App  ()   {
+  return (
+    <div>
+      <h1>prakash techincal netflix pick</h1>
+      <p>List of Best series:</p>
+      <ul>
+        <li>Dark</li>
+        <li>Extra curricular</li>
+        <li>my Holo love</li>
+        <li> my first-2 love</li>
+        <li>mr Robot</li>
+      </ul>
+    </div>
+  );
+
+}
+
+export default App;
