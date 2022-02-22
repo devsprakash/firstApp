32a32220e4d9edeefb748ababbff740142b5e90d
devPrakash7 <psamantaray77@gmail.com>
2022-02-22 11:23:54 +0530

    init

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,28 @@
 import './App.css';
 
 function App  ()   {
+      
+     let currdate = new Date(2022,1,2,8);
+     currdate = currdate.getHours();
+
+     let change = " ";
+
+     const cssstyle = { };
+
+     if(currdate >= 1 && currdate < 12){
+     change="good morning";
+      cssstyle.color='green';
+    }else if(currdate >=12 && currdate < 19 ){
+
+      change="good afternoon";
+     cssstyle.color='yellow';
+     }else{
+      change = "good night";
+      cssstyle.color = 'black';
+     } 
   return (
     <div>
-      <h1>prakash techincal netflix pick</h1>
-      <p>List of Best series:</p>
-      <ul>
-        <li>Dark</li>
-        <li>Extra curricular</li>
-        <li>my Holo love</li>
-        <li> my first-2 love</li>
-        <li>mr Robot</li>
-      </ul>
+      <h1 style={{color:'orange'}}>Hello sir,<span style={cssstyle}>{change}</span></h1>
     </div>
   );
 
